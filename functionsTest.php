a463150cc4225563287ea8693e607180f1ee056b
martinezhenry <henry.martinezd@gmail.com>
2016-05-03 16:39:42 -0400

    functions testing

--- functionsTest.php
+++ functionsTest.php
@@ -15,13 +15,23 @@ require_once './controllers/usuario.php';
 require_once './controllers/comment.php';
 
 //header("Content-Type: application/json; charset=utf-8");
-echo "<br>Albums<br>";
+echo "<br> <a href='?album'>get Albums</a><br>";
+if (isset($_GET['album'])){
 echo getAlbum();
-echo "<br>Cities<br>";
+}
+echo "<br> <a href='?city'>get Cities</a><br>";
+if (isset($_GET['city'])){
 echo getCity();
-echo "<br>Comments<br>";
+}
+echo "<br> <a href='?comment'>get Comments</a><br>";
+if (isset($_GET['comment'])){
 echo getComment();
-echo "<br>Countries<br>";
+}
+echo "<br> <a href='?country'>get Countries</a><br>";
+if (isset($_GET['country'])){
 echo utf8_decode( getCountry() );
-echo "<br>Users<br>";
+}
+echo "<br> <a href='?user'>get Users</a><br>";
+if (isset($_GET['user'])){
 echo getUsuario();
+}
